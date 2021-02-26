e7765d5cb2cdb06a5e6ca40b0b7c976f68881064
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-26 17:06:08 -0300

    refactor: remove constructor and attributes

--- src/CreateNode.ts
+++ src/CreateNode.ts
@@ -1,21 +1,3 @@
 export abstract class CreateNode {
-  value: number
-  operator: string
-  left: CreateNode | null
-  right: CreateNode | null
-
-  constructor(
-    value: number,
-    operator: string,
-    left: CreateNode | null,
-    right: CreateNode | null
-  ) 
-  {
-    this.value = value
-    this.operator = operator
-    this.left = left
-    this.right = right
-  }
-
   abstract evaluate(): number
 }
