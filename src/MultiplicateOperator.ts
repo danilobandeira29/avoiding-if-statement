a792f8e8677145960718d24bdcbc0c682492a183
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-26 17:07:08 -0300

    refactor: type node in the constructor

--- src/MultiplicateOperator.ts
+++ src/MultiplicateOperator.ts
@@ -1,7 +1,8 @@
 import { OperatorNode } from "./OperatorNode";
+import { ValueNode } from './ValueNode'
 
 export class MultiplicateOperator extends OperatorNode {
-  constructor(left: OperatorNode, right: OperatorNode) {
+  constructor(left: ValueNode, right: ValueNode) {
     super('*', left, right)
   }
 
